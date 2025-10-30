2d531e91a1c8478e3adc6599abdc1671404a4678
Nina Matthias <nina@zedat.fu-berlin.de>
2025-10-30 15:50:17 +0100

    Get location and print coordinates on screen

---
+++ gps.js
@@ -0,0 +1,19 @@
+function getPosition() {
+    return new Promise((resolve, reject) => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                resolve(position);
+            }, (error) => {
+                reject("Could not retrieve location: " + error.message);
+            });
+        } else {
+            reject("No geolocation API");
+        }
+    })
+}
+
+async function loadLocation(element) {
+    let position = await getPosition();
+    console.log(position);
+    element.innerText = `Geo-Koordinaten: ${position.coords.latitude} ${position.coords.longitude}`;
+}
